cd6de4ca6fac5b968e003617e61b1b6a7ad86fd1
kyahweng <roweljularbal0@gmail.com>
2023-05-18 20:12:36 +0100

    Add python file

---
+++ python101_project.py
@@ -0,0 +1,40 @@
+import random
+
+
+yourHand = None
+
+randomHand = None
+
+
+while True:
+    
+    yourHand = input("Rock, Paper or Scissor?")
+    randomHand = random.choice(['Rock', 'Paper', 'Scissors'])
+    print(f"Computer's hand is {randomHand}")
+
+    if yourHand.lower() == randomHand.lower():
+        print(f"You both chose {yourHand}. Play again")
+        continue
+
+    elif yourHand.lower() == 'rock' and randomHand.lower() == 'scissors':
+        print(f"You win! {yourHand} wins over {randomHand}!")
+        break
+    elif yourHand.lower() == 'scissors' and randomHand.lower() == 'paper':
+        print(f"You win! {yourHand} wins over {randomHand}!")
+        break
+    elif yourHand.lower() == 'paper' and randomHand.lower() == 'rock':
+        print(f"You win! {yourHand} wins over {randomHand}!")
+        break
+   
+    else:
+        print("You lose! Choose again")
+        continue
+    
+
+
+
+#print(f"You win! {yourHand} wins over {randomHand}!")
+
+    
+    
+   
