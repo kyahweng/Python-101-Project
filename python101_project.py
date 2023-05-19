491c9b9b11d523f01d6343835bdf79024f7d08ef
kyahweng <roweljularbal0@gmail.com>
2023-05-19 17:59:52 +0100

    specificity of the input and quit option

--- python101_project.py
+++ python101_project.py
@@ -10,24 +10,38 @@ while True:
     
     yourHand = input("Rock, Paper or Scissor?")
     randomHand = random.choice(['Rock', 'Paper', 'Scissors'])
-    print(f"Computer's hand is {randomHand}")
+   
 
     if yourHand.lower() == randomHand.lower():
-        print(f"You both chose {yourHand}. Play again")
+        print(f"Play again! You both chose {yourHand}.  ('Q' to Quit)")
+
+        continue
+
+    elif yourHand.lower() != 'rock'and yourHand.lower() != 'scissors'and yourHand.lower() != 'paper'and yourHand.lower() != "q":
+        print("Please enter the correct option")
         continue
 
     elif yourHand.lower() == 'rock' and randomHand.lower() == 'scissors':
+        print(f"Computer's hand is {randomHand}")
         print(f"You win! {yourHand} wins over {randomHand}!")
         break
     elif yourHand.lower() == 'scissors' and randomHand.lower() == 'paper':
+        print(f"Computer's hand is {randomHand}")
         print(f"You win! {yourHand} wins over {randomHand}!")
         break
     elif yourHand.lower() == 'paper' and randomHand.lower() == 'rock':
+        print(f"Computer's hand is {randomHand}")
         print(f"You win! {yourHand} wins over {randomHand}!")
+        break
+
+    
+
+    elif yourHand.lower() == "q":
+        print('Thank you for playing')
         break
    
     else:
-        print("You lose! Choose again")
+        print('You lose! Choose again ("Q" to Quit)')
         continue
     
 
